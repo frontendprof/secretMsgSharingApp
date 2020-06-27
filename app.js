161f5175e6d20c2d1055029736acf4354d8aa401
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-27 23:56:15 +0500

    Generating the URL

--- app.js
+++ app.js
@@ -3,7 +3,15 @@
 document.querySelector("form").addEventListener("submit",e=>{
     e.preventDefault();
 
-    const inputVal=document.querySelector("input");
-    console.log(inputVal.value);
+    const inputVal=document.querySelector("#msg-input");
+    const encrypted=btoa(inputVal.value);
+    
+
+
+    const inputLink=document.querySelector("#link-input");
+    inputLink.value=`${window.location}#${encrypted}`;
+    inputLink.select();
+
+
     
 })
