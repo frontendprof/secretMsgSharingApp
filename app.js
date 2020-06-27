c93ccf9feff72f4d06faceb8a720de68e0ef5298
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-28 00:31:25 +0500

    Desgining

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 document.querySelector("form").addEventListener("submit",e=>{
     e.preventDefault();
 
+    document.querySelector("#msg-form").classList.add("hide");
+    document.querySelector("#link-form").classList.remove("hide");
+
     const inputVal=document.querySelector("#msg-input");
     const encrypted=btoa(inputVal.value);
     
